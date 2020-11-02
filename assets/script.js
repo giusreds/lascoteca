f5166cac46da56874137936743a68dc89d761107
Giuseppe Rossi <giuseppe-car@live.it>
2020-11-02 12:47:24 +0100

    Changed game load mode
    INDEX now detects if page is loaded from GitHub and loads the game externally.
    Also changed icons.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
 // (c)2020 Giuseppe Rossi
 
+// URL GitHub
+var gh_url = "giusreds.github.io";
+// URL Gioco
+var game_url = "https://rebrand.ly/lascoteca-origin";
+
+// Se la pagina e' caricata da GitHub, 
+// carico il gioco dal CDN di itch.io
+$(document).ready(function () {
+    if (window.location.href.includes(gh_url))
+        $("#game").attr("src", game_url);
+});
+
+// Il gioco e' stato caricato
 window.addEventListener("message", function (event) {
     var message = event.data;
     if (message == "loaded") {
-        // document.getElementById("loading").style.display = "none";
         $("#loading").fadeOut(200);
         $("#game").focus();
     }
