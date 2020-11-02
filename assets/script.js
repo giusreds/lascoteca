37e64a5a0dc4c7b4e05e67d8e27d46d7af673866
Giuseppe Rossi <giuseppe-car@live.it>
2020-11-02 21:45:46 +0100

    Changed jQuery syntax

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var game_url = "https://rebrand.ly/lascoteca-origin";
 
 // Se la pagina e' caricata da GitHub, 
 // carico il gioco dal CDN di itch.io
-$(document).ready(function () {
+$(function () {
     if (window.location.href.includes(gh_url))
         $("#game").attr("src", game_url);
 });
