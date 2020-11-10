862cb0532a442c28977810b51a8c226804398701
Giuseppe Rossi <giuseppe-car@live.it>
2020-11-10 13:49:43 +0100

    Added libraries and changed url fetch method

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,29 @@
 
 // URL GitHub
 var gh_url = "giusreds.github.io";
-// URL Gioco
-var game_url = "https://rebrand.ly/lascoteca-origin";
+// URL Richiesta
+var request_url = "https://tinyurl.com/giusreds-url?goto=lascoteca-origin";
 
 // Se la pagina e' caricata da GitHub, 
 // carico il gioco dal CDN di itch.io
 $(document).ready(function () {
+    // Ottengo l'URL del gioco e lo memorizzo in un cookie
+    $.ajax({
+        url: request_url,
+        success: function (data) {
+            Cookies.set('lascoteca-origin', data, { expires: 20, path: '' });
+        }
+    });
+    // Imposto la sorgente dell'iframe
     if (window.location.href.includes(gh_url))
-        $("#game").attr("src", game_url);
+        var retry = setInterval(function () {
+            if (Cookies.get('lascoteca-origin')) {
+                $("#game").attr("src", Cookies.get('lascoteca-origin'));
+                clearInterval(retry);
+            }
+        }, 50);
     else
         $("#game").attr("src", "resources/index.html");
-});
-
-// Controllo se si e' offline
-$("#game").ready(function () {
-    if (!navigator.onLine)
-        $("#loading").hide();
 });
 
 // Il gioco e' stato caricato
